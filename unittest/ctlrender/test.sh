de3fab983082f2483a9e8aecb87e8d7ade6d4dcf
michaeldsmith <37905569+michaeldsmith@users.noreply.github.com>
2022-11-14 21:54:17 -0800

    update ctlrender unit test, fixes ampas#102

--- unittest/ctlrender/test.sh
+++ unittest/ctlrender/test.sh
@@ -8,10 +8,20 @@ for J in tiff8 tiff16 tiff32 dpx8 dpx10 dpx12 dpx16; do
 		name=`echo $I | sed -e 's/\..*//'`
 		ext=`echo $J | sed -e 's/[0-9]//g'`
 		echo ${I} '->' ${J}
-		$CTLRENDER -ctl unity.ctl -format ${J} -force ${I} output/${name}_${J}.${ext}
+		if [[ $IS_TIFF_FOUND == 0 ]] && [[ $J == *"tif"* ]] ; then
+			printf 'skipping %s because no tiff\n' $J
+		elif [[ $IS_TIFF_FOUND == 0 ]] && [[ $I == *"tif"* ]] ; then
+			printf 'skipping %s because no tiff\n' $I
+		else
+			$CTLRENDER -ctl unity.ctl -format ${J} -force ${I} output/${name}_${J}.${ext}
+		fi
 	done
+
 	echo ${J} unity test
-	$CTLRENDER -ctl unity.ctl -format ${J} -force bars_photoshop_32_be_planar.tif output/bars_tiff32_${J}.${ext}
-	$CTLRENDER -ctl unity.ctl -format tiff32 -force output/bars_tiff32_${J}.${ext} output/bars_tiff32_${J}_tiff32.tiff
+
+	if [[ $IS_TIFF_FOUND == 1 ]] ; then
+		$CTLRENDER -ctl unity.ctl -format ${J} -force bars_photoshop_32_be_planar.tif output/bars_tiff32_${J}.${ext}
+		$CTLRENDER -ctl unity.ctl -format tiff32 -force output/bars_tiff32_${J}.${ext} output/bars_tiff32_${J}_tiff32.tiff
+	fi
 done
 
